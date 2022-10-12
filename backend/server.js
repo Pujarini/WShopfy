4c6dbe7b5ced20f13eeeb0c2b6b039b70fb30e1a
Pujarini Jena <pujarinijena@Pujarini-MacBook-Pro.local>
2022-10-13 01:09:00 +0530

    server file modified

--- backend/server.js
+++ backend/server.js
@@ -31,7 +31,7 @@ const __dirname = path.resolve();
 app.use("/uploads", express.static(path.join(__dirname, "/uploads"))); //make a file static
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/build/")));
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
 
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
