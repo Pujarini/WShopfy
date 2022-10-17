885a454ba95104647c3bf38cde5818e2d47b68fa
Pujarini Jena <pujarinijena@Pujarini-MacBook-Pro.local>
2022-10-17 16:41:49 +0530

    added prod build scripts

--- backend/server.js
+++ backend/server.js
@@ -31,7 +31,7 @@ const __dirname = path.resolve();
 app.use("/uploads", express.static(path.join(__dirname, "/uploads"))); //make a file static
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/build")));
+  app.use(express.static(path.join(__dirname, "../frontend/build")));
 
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
@@ -46,7 +46,7 @@ app.use(notFound);
 
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5000;
 
 app.listen(
   PORT,
